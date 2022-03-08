665f4776e7065a274631f549b050b9e0a3896637
xuahugk2 <xuanhung482k@gmail.com>
2022-03-08 09:17:28 +0700

    sua name input

--- kiemtra/kiemtra/Form1.Designer.cs
+++ kiemtra/kiemtra/Form1.Designer.cs
@@ -30,9 +30,9 @@ namespace kiemtra
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textboxusername = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
             this.btblogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -40,10 +40,9 @@ namespace kiemtra
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(422, 33);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(563, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 36);
+            this.label1.Size = new System.Drawing.Size(118, 45);
             this.label1.TabIndex = 0;
             this.label1.Text = "Login";
             // 
@@ -51,66 +50,64 @@ namespace kiemtra
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(62, 124);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(83, 153);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(222, 36);
+            this.label2.Size = new System.Drawing.Size(273, 45);
             this.label2.TabIndex = 0;
             this.label2.Text = "Tên đăng nhập";
             // 
-            // textBox1
+            // textboxusername
             // 
-            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(305, 121);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(417, 44);
-            this.textBox1.TabIndex = 1;
+            this.textboxusername.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxusername.Location = new System.Drawing.Point(407, 149);
+            this.textboxusername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textboxusername.Name = "textboxusername";
+            this.textboxusername.Size = new System.Drawing.Size(555, 53);
+            this.textboxusername.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(62, 228);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(83, 281);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(150, 36);
+            this.label3.Size = new System.Drawing.Size(186, 45);
             this.label3.TabIndex = 2;
             this.label3.Text = "Mật khẩu";
             // 
-            // txtpassword
+            // txtmatkhau
             // 
-            this.txtpassword.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtpassword.Location = new System.Drawing.Point(305, 220);
-            this.txtpassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtpassword.Name = "txtpassword";
-            this.txtpassword.PasswordChar = '*';
-            this.txtpassword.Size = new System.Drawing.Size(417, 44);
-            this.txtpassword.TabIndex = 3;
+            this.txtmatkhau.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmatkhau.Location = new System.Drawing.Point(407, 271);
+            this.txtmatkhau.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.PasswordChar = '*';
+            this.txtmatkhau.Size = new System.Drawing.Size(555, 53);
+            this.txtmatkhau.TabIndex = 3;
             // 
             // btblogin
             // 
             this.btblogin.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btblogin.Location = new System.Drawing.Point(357, 315);
-            this.btblogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btblogin.Location = new System.Drawing.Point(476, 388);
+            this.btblogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btblogin.Name = "btblogin";
-            this.btblogin.Size = new System.Drawing.Size(214, 61);
+            this.btblogin.Size = new System.Drawing.Size(285, 75);
             this.btblogin.TabIndex = 4;
             this.btblogin.Text = "Đăng nhập";
             this.btblogin.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(838, 469);
+            this.ClientSize = new System.Drawing.Size(1117, 577);
             this.Controls.Add(this.btblogin);
-            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtmatkhau);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textboxusername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -122,9 +119,9 @@ namespace kiemtra
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textboxusername;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtmatkhau;
         private System.Windows.Forms.Button btblogin;
     }
 }
